bdadef533dcd8454a9ade1138b4a94fc0d12082b
Aluno SENAI <Aluno@SN-358692.edu.fiesc.com.br>
2025-04-15 10:44:20 -0300

    aumento do board

---
+++ space.js
@@ -0,0 +1,17 @@
+//board
+let tileSize = 32;
+let rows = 16;
+let columns = 16;
+
+let board;
+let boardWidth = tileSize * columns // 32 * 16
+let boardHeight = tileSize * rows // 32 * 16
+let context;
+
+windows.onload = function() {
+    board = document.getElementById("board");
+    board.width = boardWidth;
+    board.height = boardHeight
+    context = board.getContext("2d"); // pra desenhar
+    
+}
