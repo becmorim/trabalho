721a61b91923e0b001f94e38dcf0a8907d37f69b
Aluno SENAI <Aluno@SN-358692.edu.fiesc.com.br>
2025-04-15 10:59:33 -0300

    ajustes do x e y

--- space.js
+++ space.js
@@ -8,10 +8,25 @@ let boardWidth = tileSize * columns // 32 * 16
 let boardHeight = tileSize * rows // 32 * 16
 let context;
 
+//ship
+let shipWidth = tileSize*2;
+let shipHeight = tileSize;
+let shipX = tileSize * columns/2 - tileSize;
+let shipY = tileSize * rows - tileSize*2;
+
+let ship = {
+    x : shipX,
+    y : shipY,
+    width : shipWidth,
+    height : shipHeight
+}
+
 windows.onload = function() {
     board = document.getElementById("board");
     board.width = boardWidth;
     board.height = boardHeight
     context = board.getContext("2d"); // pra desenhar
-    
+  // nave
+  context.fillStyle="green";
+  context.fillRect(ship.x, ship.y, ship.width, ship.height);
 }
